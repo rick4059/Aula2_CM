d39aeab649bf5cb7d596970c79ae27601e58da44
ricky <ricky19ferreira@gmail.com>
2022-04-01 12:13:42 +0100

    Precistencia de dados utilizando o sharedPreferences

--- app/src/main/java/com/example/aula2/MainActivity.kt
+++ app/src/main/java/com/example/aula2/MainActivity.kt
@@ -1,41 +1,82 @@
 package com.example.aula2
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.ActionMode
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("life cicle: onCreate")
 
-        Toast.makeText(this, R.string.Welcome_Message, Toast.LENGTH_SHORT).show()
+        val sharedPreference: SharedPreferences = getSharedPreferences("FILE_1", Context.MODE_PRIVATE)
+
+        val usernameValue = sharedPreference.getString("PREF_USERNAME", "")
+
+        Log.d("SHARED_PREF_AULA", "Found Shared pref: $usernameValue")
+
+        if(usernameValue!!.isNotEmpty()){
+            showWelcome(usernameValue)
+        }
+
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        println("life cicle: onStart")
+    private fun showWelcome(usernameValue: String) {
+        findViewById<TextView>(R.id.tv1).text = "Hello, $usernameValue"
+        findViewById<EditText>(R.id.et1).isEnabled = false
+
+
+        val btn = findViewById<Button>(R.id.btn1)
+        btn.text = "logout"
+
+        btn.setOnClickListener{
+            logout(it)
+        }
     }
 
-    override fun onPause() {
-        super.onPause()
-        println("life cicle: onPause")
+    private fun logout(it: View?) {
+        val sharedPreference: SharedPreferences = getSharedPreferences("FILE_1", Context.MODE_PRIVATE)
+        sharedPreference.edit().clear().apply()
+
+        showLogin()
+        Log.d("SHARED_PREF_AULA", "Found Shared pref: ")
     }
 
-    override fun onStop() {
-        super.onStop()
-        println("life cicle: onStop")
+    private fun showLogin() {
+        findViewById<TextView>(R.id.tv1).text = "Hello"
+        findViewById<EditText>(R.id.et1).isEnabled = true
+
+
+        val btn = findViewById<Button>(R.id.btn1)
+        btn.text = "Login"
+
+        btn.setOnClickListener{
+            Login(it)
+        }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        println("life cicle: onDestroy")
+    fun Login(view: View) {
+        val editTextUsername = findViewById<EditText>(R.id.et1)
+
+        if(editTextUsername.text.isNotEmpty()){
+            val sharedPreference: SharedPreferences = getSharedPreferences("FILE_1", Context.MODE_PRIVATE)
+            sharedPreference.edit()
+                .putString("PREF_USERNAME", editTextUsername.text.toString())
+                .apply()
+
+            showWelcome(editTextUsername.text.toString())
+            Log.d("SHARED_PREF_AULA", "Found Shared pref: ${editTextUsername.text}")
+        }
     }
 
-    override fun onRestart() {
-        super.onRestart()
-        println("life cicle: onRestart")
-    }
+
 }
