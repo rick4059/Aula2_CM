099720e0910b269e020d1542b52bddbcf43e7f42
ricky <ricky19ferreira@gmail.com>
2022-04-28 09:51:22 +0100

    Room

--- app/src/main/java/com/example/aula2/MainActivity.kt
+++ app/src/main/java/com/example/aula2/MainActivity.kt
@@ -9,33 +9,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("life cicle: onCreate")
 
-        Toast.makeText(this, R.string.Welcome_Message, Toast.LENGTH_SHORT).show()
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        println("life cicle: onStart")
-    }
 
-    override fun onPause() {
-        super.onPause()
-        println("life cicle: onPause")
-    }
-
-    override fun onStop() {
-        super.onStop()
-        println("life cicle: onStop")
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        println("life cicle: onDestroy")
-    }
-
-    override fun onRestart() {
-        super.onRestart()
-        println("life cicle: onRestart")
-    }
 }
