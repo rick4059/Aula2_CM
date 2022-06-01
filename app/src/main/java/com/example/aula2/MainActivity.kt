58e39deff0e37431041e47da7964e14779dcb5b3
rick4059 <94701552+rick4059@users.noreply.github.com>
2022-06-01 19:07:08 +0100

    Maps

--- app/src/main/java/com/example/aula2/MainActivity.kt
+++ app/src/main/java/com/example/aula2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ActionMode
+import android.view.View
 import android.widget.Button
 import android.widget.Toast
 
@@ -16,11 +17,11 @@ class MainActivity : AppCompatActivity() {
         Toast.makeText(this, R.string.Welcome_Message, Toast.LENGTH_SHORT).show()
     }
 
-    val btn = findViewById<Button>(R.id.btn1)
-
-
+    fun butao(view: View) {
         val intent = Intent(this, MapsActivity::class.java)
         startActivity(intent)
+    }
+
 
 
 }
